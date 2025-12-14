cca8a94579958436bf2bed280d5f5014a441edee
Pál Dániel <daniel.pal@adaptiverecognition.tech>
2025-12-14 06:40:00 +0100

    [Satisfactory] TODO: calc plan, avoid loops [Dyson Sphere Program]

---
+++ dsp/index.js
@@ -0,0 +1,278 @@
+let g_data_json = g_dsp_data;
+let g_content = null;
+
+const image_dir = "../images/dsp";
+const key_field_name = "key";
+
+
+function table_begin(o) {o+="<table>";}
+function table_end(o) {o+="</table>";}
+
+function table_row_begin(o) {o+="<tr>";}
+function table_row_end(o) {o+="</tr>";}
+
+function table_col_begin(o) {o+="<td>";}
+function table_col_end(o) {o+="</td>";}
+
+function table_add_header(o, cols) { table_begin(o); for(let elem in cols) {table_col_begin(o); o+=elem; table_col_end(o);} table_end(o);}
+function table_add_row(o, cols) {table_begin(o); for(let elem in cols) {table_col_begin(o); o+=elem; table_col_end(o);} table_end(o);}
+
+function is_resource(item_key) {
+	const resources = g_data_json["resources"];
+	for(let i=0;i<resources.length;++i) {
+		const r = resources[i];
+		//if(r.key == item_key) {
+		if(r.item == item_key) {
+			return true;
+		}
+	}
+	return false;
+}
+
+function find_item_recipe(item_key) {
+	let recipes = g_data_json["recipes"];
+	for(let i = 0; i < recipes.length; ++i) {
+		let recipe = recipes[i];
+		let products = recipe.products;
+		if(products[0][0] != item_key) continue;
+		/*
+		if(!filter_recipe(recipe)) {
+			continue;
+		}*/
+		return recipe;
+	}
+	return null;
+}
+
+function find_item_by_key_name(key) {
+	let items = g_data_json["items"];
+	for(let i = 0; i < items.length; ++i) {
+		if(items[i][key_field_name] != key) continue;
+		return items[i];
+	}
+	return null;
+}
+
+function find_item_name_by_key_name(key) {
+	let item = find_item_by_key_name(key);
+	if(item == null) return null;
+	return item.name;
+}
+
+function get_image_str_for_item(name) {
+	if(name == null) {
+		return "";
+	}
+	return "<img width='48' src='" + image_dir + "/"+name+".png' />";
+}
+
+function get_image_str_for_item_key(key_name) {
+	let str = "";
+	str += "<a href='#" + "item_row_" + key_name + "'>";
+	str += get_image_str_for_item(find_item_name_by_key_name(key_name));
+	str += "</a>";
+	return str;
+}
+
+
+function find_recipes_for_product(product_name) {
+	let result = [];
+	const recipes = g_data_json.recipes;
+	for(let i=0;i<recipes.length;++i) {
+		const r = recipes[i];
+		const products = r.products;
+		for(let j=0;j<products.length;++j) {
+			const p = products[j];
+			if(p.name == product_name) {
+				result.push(r);
+			}
+		}
+	}
+	
+	return result;
+}
+
+function find_recipes_for_ingredient(item_key) {
+	let result = [];
+	const recipes = g_data_json.recipes;
+	for(let i=0;i<recipes.length;++i) {
+		const r = recipes[i];
+		
+		// filter buildings
+		const item = find_item_by_key_name(r.products[0].name);
+		if(item.category == "building") {
+			continue;
+		}
+		
+		const ingredients = r.ingredients;
+		for(let j=0;j<ingredients.length;++j) {
+			const ing = ingredients[j];
+			if(ing.name == item_key) {
+				result.push(r);
+			}
+		}
+	}
+	
+	return result;
+}
+
+
+function create_ingredient_column(item_list, time) {
+	let str = "";
+	for(let i in item_list) {
+		const curItem = item_list[i];
+		const name = curItem.name;
+		const amount = curItem.amount;
+		str += get_image_str_for_item_key(name);
+		str += amount + " (" + (amount*(60.0/time)).toFixed(0) + ")";
+	}
+	return str;
+}
+
+function create_product_column(item_list, time) {
+	return create_ingredient_column(item_list, time);
+}
+
+function create_recipe_row(rec) {
+	let str = "";
+	str += "<td>";
+	str += rec.name + " (" + rec.key + ")";
+	str += "</td>";
+	str += "<td>";
+	str += rec.time /*.toFixed(2)*/ + " s";
+	str += "</td>";
+	str += "<td>";
+	str += create_product_column(rec.products, rec.time);
+	str += "</td>";
+	str += "<td>";
+	str += create_ingredient_column(rec.ingredients, rec.time);
+	str += "</td>";
+	return str;
+}
+
+/*
+function create_recipe_list() {
+	let div = document.createElement("div");
+	let str = "<table>";
+	//str += "<tr><th>img</th><th>product</th><th>speed</th></tr>";
+	
+	let recipes = g_data_json["recipes"];
+	
+	for(let ix in recipes) {
+		const curRec = recipes[ix];
+		const time = curRec.time;
+		const main_prod = curRec.products[0];
+		const main_prod_item = find_item_by_key_name(main_prod.name);
+		const is_building = main_prod_item.category == "building";
+		const is_building_class = is_building ? " class=\"building\" " : "";
+		str += "<tr" + is_building_class + ">";
+		str += "<td>" + curRec.name + "</td>";
+		str += "<td>";
+		str += create_product_column(curRec.products, time);
+		str += "</td>";
+		str += "<td>";
+		str += create_ingredient_column(curRec.ingredients, time);
+		str += "</td>";
+		str += "</tr>";
+	}
+	str += "</table>";
+	div.innerHTML = str;
+	return div;
+}*/
+
+
+function create_recipe_list_table(recipes) {
+	str = "";
+	str += "<table>";
+	for(let ii=0;ii<recipes.length;++ii) {
+		const rr = recipes[ii];
+		str += "<tr>";
+		str += create_recipe_row(rr);
+		str += "</tr>";
+	}
+	str += "</table>";
+	return str;
+}
+
+function create_item_list() {
+	let div = document.createElement("div");
+	let str = "<table>";
+	//str += "<tr><th>img</th><th>product</th><th>speed</th></tr>";
+	
+	const recipes = g_data_json["recipes"];
+	const items = g_data_json.items;
+	
+	for(let ix in items) {
+		const curItem = items[ix];
+		const itemName = curItem.name;
+		const itemKey = curItem.key;
+		const category = curItem.category;
+		const row = curItem.row;
+		
+		const is_building = category == "building";
+		let isResource = false;
+		if(!is_building) {
+			isResource = is_resource(itemKey);
+		}
+		
+		const item_recipes = find_recipes_for_product(itemKey);
+		
+		//const time = curRec.time;
+		
+		let classes = [];
+		if(is_building) classes.push("building");
+		if(isResource) classes.push("resource");
+		
+		const image_str = get_image_str_for_item_key(itemKey);
+		const row_id = "item_row_" + itemKey;
+		
+		str += "<tr" + " id=\"" + row_id + "\"" + " class=\"" + classes.join(" ") + "\" >";
+			str += "<td>" + itemName + " (" + itemKey + ")" + "</td>";
+			
+			str += "<td>";
+			str += image_str;
+			str += "</td>";
+			
+			str += "<td>";
+			str += create_recipe_list_table(item_recipes);
+			str += "</td>";
+			
+			str += "<td>";
+			str += create_recipe_list_table(find_recipes_for_ingredient(itemKey));
+			str += "</td>";
+		str += "</tr>";
+	}
+	str += "</table>";
+	div.innerHTML = str;
+	return div;
+}
+
+
+function create_plan(target) {
+	const recipes = g_data_json.recipes;
+	
+	let plan = {};
+	let needs = target.slice(); // no ref
+	
+	while(needs.length > 0) {
+		const product_and_amount = needs.pop();
+		const product = product_and_amount[0];
+		const amount = product_and_amount[1];
+		let r = find_recipes_for_product(product);
+		// TODO
+		console.log(r);
+	}
+	return plan;
+}
+
+function init_page() {
+	g_content = document.getElementById("content");
+	//g_content.appendChild(create_recipe_list());
+	g_content.appendChild(create_item_list());
+	
+	let target = [["un", 100]]; // universe matrix, 100/min
+	
+	console.log(create_plan(target));
+	
+	//console.log(find_recipes_for_product("un"));
+}
--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 // const g_satisfactory_data from js instead of JSON
 let g_content = null;
 let g_product_target_div = null;
-//let g_data_json = null;
+let g_data_json = null;
+
+const image_dir = "images";
+const key_field_name = "key_name";
+g_data_json = g_satisfactory_data;
+
+/*
+const image_dir = "images/dsp";
+const key_field_name = "key";
+g_data_json = g_dsp_data;
+*/
+
 
 
 const g_final_target =
@@ -12,6 +23,16 @@ const g_final_target =
   ["ballistic-warp-drive", 200]
 ];
 
+
+/*
+const g_final_target =
+[
+  ["um", 1000]
+  //["...sculptor", 1000],
+  //["ai-expansion-server", 256],
+  //["ballistic-warp-drive", 200]
+];
+*/
 const g_target_hours = 20;
 
 
@@ -28,15 +49,86 @@ function table_add_header(o, cols) { table_begin(o); for(let elem in cols) {tabl
 function table_add_row(o, cols) {table_begin(o); for(let elem in cols) {table_col_begin(o); o+=elem; table_col_end(o);} table_end(o);}
 
 
+
+function is_resource(item_name) {
+	
+	const resources = g_data_json.resources;
+	
+	for(let ix in resources) {
+		const r = resources[ix];
+		if(r[key_field_name] == item_name) {
+			return true;
+		}
+	}
+	return false;
+	/*
+	if(item_name.endsWith("-ore")) {
+		return true;
+	}
+	const ores = [
+		"coal",
+		"sulfur",
+		"limestone",
+		"bauxite",
+		
+		"water",
+		"crude-oil",
+		"nitrogen-gas"
+		];
+	if(ores.includes(item_name)) {
+		return true;
+	}
+	return false;
+	*/
+}
+
+function is_resource_recipe(recipe) {
+	return (recipe.ingredients.length == 0) || is_resource(recipe.products[0][0]);
+}
+
+function filter_recipe(recipe) {
+	
+	if(is_resource_recipe(recipe)) {
+		return false;
+	}
+	if(recipe.category == "converting") {
+		return false;
+	}
+	if(recipe.name.startsWith("alt-")) {
+		return false;
+	}
+	if(recipe.name.startsWith("reanimated-sam")) {
+		return false;
+	}
+	return true;
+}
+
+function find_item_recipe(item_key) {
+	let recipes = g_data_json["recipes"];
+	for(let i = 0; i < recipes.length; ++i) {
+		let recipe = recipes[i];
+		let products = recipe.products;
+		if(products[0][0] != item_key) continue;
+		if(!filter_recipe(recipe)) {
+			continue;
+		}
+		return recipe;
+	}
+	return null;
+}
+
 function find_item_name_by_key_name(key_name) {
-	let items = g_satisfactory_data["items"];
+	let items = g_data_json["items"];
 	for(let i = 0; i < items.length; ++i) {
-		if(items[i].key_name != key_name) continue;
+		if(items[i][key_field_name] != key_name) continue;
 		return items[i].name;
 	}
-	let fluids = g_satisfactory_data["fluids"];
+	if(!("fluids" in g_data_json)) {
+		return null;
+	}
+	let fluids = g_data_json["fluids"];
 	for(let i = 0; i < fluids.length; ++i) {
-		if(fluids[i].key_name != key_name) continue;
+		if(fluids[i][key_field_name] != key_name) continue;
 		return fluids[i].name;
 	}
 	return null;
@@ -46,7 +138,7 @@ function get_image_str_for_item(name) {
 	if(name == null) {
 		return "";
 	}
-	return "<img width='64' src='images/"+name+".png' />";
+	return "<img width='32' src='" + image_dir + "/"+name+".png' />";
 }
 
 function get_image_str_for_item_key(key_name) {
@@ -84,31 +176,34 @@ function create_product_target_div() {
 
 // function load_data_json() {}
 
+function create_recipe_column(item_list, time) {
+	let str = "";
+	for(let i=0;i<item_list.length;++i) {
+		const curItem = item_list[i];
+		str += get_image_str_for_item_key(curItem[0]);
+		str += curItem[1] + " (" + (curItem[1]*(60.0/time)).toFixed(0) + ")";
+	}
+	return str;
+}
+
 function create_recipe_list() {
 	let div = document.createElement("div");
 	let str = "<table>";
 	//str += "<tr><th>img</th><th>product</th><th>speed</th></tr>";
 	
-	let recipes = g_satisfactory_data["recipes"];
+	let recipes = g_data_json["recipes"];
 	
 	for(let ix in recipes) {
+		str += "<tr>";
 		let curRec = recipes[ix];
 		let time = curRec.time;
 		//let main_product = cur.products[0][0];
 		str += "<td>" + curRec.name + "</td>";
 		str += "<td>";
-		for(let i=0;i<curRec.products.length;++i) {
-			let curItem = curRec.products[i];
-			str += get_image_str_for_item_key(curItem[0]);
-			str += curItem[1] + " (" + (curItem[1]*(60.0/time)).toFixed(0) + ")";
-		}
+		str += create_recipe_column(curRec.products, time);
 		str += "</td>";
 		str += "<td>";
-		for(let i=0;i<curRec.ingredients.length;++i) {
-			let curItem = curRec.ingredients[i];
-			str += get_image_str_for_item_key(curItem[0]);
-			str += curItem[1] + " (" + (curItem[1]*(60.0/time)).toFixed(0) + ")";
-		}
+		str += create_recipe_column(curRec.ingredients, time);
 		str += "</td>";
 		str += "</tr>";
 	}
@@ -117,8 +212,230 @@ function create_recipe_list() {
 	return div;
 }
 
+
+function add_to_item_speed_list(res, item, speed) {
+	//console.log("ADD --- " + item + " - " + speed);
+	for(let i = 0; i < res.length; ++i) {
+		let r = res[i];
+		if(r[0] == item) {
+			r[1] += speed;
+			return;
+		}
+	}
+	res.push([item, speed]);
+}
+
+
+/*
+var in_out_list = {
+	"motor": {
+		//"speed": 50,
+		"in": {"stator": 100, "rotor", 100}
+	}
+};
+*/
+
+
+function add_to_inout(res, recipe, item, speed) {
+	if(!(item in res)) {
+		res[item] = {
+			"speed": speed,
+			"recipe": recipe,
+			"in": {}
+		};
+	} else {
+		res[item].speed += speed;
+	}
+}
+
+function add_to_inout_ingredient(res, recipe, target_item, in_item, in_speed) {
+	if(!(target_item in res)) {
+		res[target_item] = {
+			"speed": 0,
+			"recipe": recipe,
+			"in": {}
+		};
+	}
+	let r = res[target_item];
+	if(in_item in r["in"]) {
+		r["in"][in_item] += in_speed;
+	} else {
+		r["in"][in_item] = in_speed;
+	}
+}
+
+function find_array_value(arr, val) {
+	for(let key in arr) {
+		if(arr[key][0] == val) {
+			return arr[key];
+		}
+	}
+	return null;
+}
+
+function fill_inout_outs(r) {
+	for(out_key in r) {
+		let recipe = r[out_key].recipe;
+		let out_speed = r[out_key].speed;
+		let product_count = null;
+		if(recipe) { product_count = recipe.products[0][1]; }
+		for(in_key in r[out_key]["in"]) {
+			if(in_key in r) {
+				let in_o = r[in_key];
+				if(!("out" in in_o)) {
+					in_o["out"] = {};
+				} 
+				let oo = in_o["out"];
+				let elem = find_array_value(recipe.ingredients, in_key);
+				let in_count = elem[1];
+				let in_speed = out_speed / product_count * in_count;
+				if(out_key in oo) {
+					oo[out_key] += in_speed;
+				} else {
+					oo[out_key] = in_speed;
+				}
+			} else {
+				// TODO
+			}
+		}
+	}
+}
+
+function check_for_loop(stack, item_key) {
+	for(let i in stack) {
+		if(stack[i][0] == item_key) {
+			return true; // loop
+		}
+	}
+	return false;
+}
+
+function print_calc_stack(needs) {
+	console.log("-----------");
+	for(let i in needs) {
+		console.log("STACK["+i+"] " + needs[i][0] + " - " + needs[i][1]);
+	}
+	console.log("-----------");
+}
+
+function calc_plan(target) {
+	const b_log = false;
+	
+	let result = [];
+	let in_out_list = {};
+	let side_products = {};
+	
+	let needs = target.slice(); // no ref
+	
+	const depth_limit = 1000;
+	const N_limit = 10000;
+	let cnt = 0;
+	
+	while(needs.length > 0) {
+		++cnt;
+		
+		if(b_log) { print_calc_stack(needs); }
+		
+		if(needs.length > depth_limit) {
+			console.log("depth_limit reached");
+			break;
+		}
+		if(cnt > N_limit) {
+			console.log("cycle limit reached");
+			break;
+		}
+		
+		let cur = needs.pop();
+		let item_key = cur[0];
+		let item_speed = cur[1];
+		add_to_item_speed_list(result, item_key, item_speed);
+		
+		/*
+		if(is_resource(item_key)) {
+			console.log("ORE - " + item_key);
+			continue;
+		}*/
+		
+		let recipe = find_item_recipe(item_key);
+		
+		if(null == recipe) {
+			console.log("recipe not found '" + item_key + "'");
+			add_to_inout(in_out_list, null, item_key, item_speed);
+			continue;
+		}
+		let product_per_recipe = recipe.products[0][1];
+		let recipe_multiplier = item_speed / product_per_recipe;
+		
+		for(let i = 1; i < recipe.products.length; ++i) {
+			let side_prod = recipe.products[i];
+			let side_speed = recipe_multiplier * side_prod[1];
+			side_products[side_prod[0]] = side_speed;
+		}
+				
+		let ingredients = recipe.ingredients;
+		
+		add_to_inout(in_out_list, recipe, item_key, item_speed);
+		
+		for(let i = 0; i < ingredients.length; ++i) {
+			let ing_key = ingredients[i][0];
+			let ing_speed = recipe_multiplier * ingredients[i][1];
+			if(check_for_loop(needs, ing_key)) {
+				console.log("LOOP!");
+			} else {
+				add_to_inout_ingredient(in_out_list, recipe, item_key, ing_key, ing_speed);
+				add_to_item_speed_list(needs, ing_key, ing_speed);
+			}
+		}
+		
+	}
+	
+	fill_inout_outs(in_out_list);
+	
+	return {
+		"list": result,
+		"inout": in_out_list,
+		"side_products": side_products
+	};
+}
+
+function calc_plan_tree(target) {
+	
+	let resource_prod = {};
+	let intermediate_prod = {};
+	let net_prod = {};
+	
+	
+	let tree = {};
+	
+	let needs = target.slice(); // no ref
+	
+	// TODO
+	
+	return tree;
+}
+
+function create_plan_div() {
+	
+}
+
+function print_plan(plan) {
+	for(let i = 0; i < plan.length; ++i) {
+		let p = plan[i];
+		let name = p[0];
+		let speed = p[1];
+		console.log(name + " - " + speed);
+	}
+}
+
 function init_page() {
 	g_content = document.getElementById("content");
 	create_product_target_div();
 	g_content.appendChild(create_recipe_list());
+	
+	//let plan = calc_plan([["motor", 100]]);
+	let plan = calc_plan([["fused-modular-frame", 1]]);
+	//let plan = calc_plan([["heavy-modular-frame", 1]]);
+	
+	console.log(plan);
+	//print_plan(plan);
 }
